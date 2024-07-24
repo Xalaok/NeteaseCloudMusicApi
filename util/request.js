96c2e3731891512bff161d691cf316e4d890e050
Xalaok <74130512+Xalaok@users.noreply.github.com>
2024-07-24 10:39:10 +0800

    v4.22.0

--- util/request.js
+++ util/request.js
@@ -15,6 +15,7 @@ const anonymous_token = fs.readFileSync(
 )
 const { URLSearchParams, URL } = require('url')
 const iosAppVersion = '9.0.65'
+const { APP_CONF } = require('../util/config.json')
 // request.debug = true // 开启可看到更详细信息
 
 const chooseUserAgent = (uaType) => {
@@ -28,7 +29,7 @@ const chooseUserAgent = (uaType) => {
   }
   return userAgentMap.pc
 }
-const createRequest = (method, url, data = {}, options) => {
+const createRequest = (method, uri, data = {}, options) => {
   const cookie = options.cookie || {}
   return new Promise((resolve, reject) => {
     let headers = {
@@ -43,8 +44,6 @@ const createRequest = (method, url, data = {}, options) => {
     }
     if (method.toUpperCase() === 'POST')
       headers['Content-Type'] = 'application/x-www-form-urlencoded'
-    if (url.includes('music.163.com'))
-      headers['Referer'] = 'https://music.163.com'
     let ip = options.realIP || options.ip || ''
     // console.log(ip)
     if (ip) {
@@ -62,7 +61,7 @@ const createRequest = (method, url, data = {}, options) => {
         appver:
           options.cookie.appver || (cookie.os != 'pc' ? iosAppVersion : ''),
       }
-      if (url.indexOf('login') === -1) {
+      if (uri.indexOf('login') === -1) {
         options.cookie['NMTID'] = CryptoJS.lib.WordArray.random(16).toString()
       }
       if (!options.cookie.MUSIC_U) {
@@ -85,50 +84,98 @@ const createRequest = (method, url, data = {}, options) => {
       headers['Cookie'] = cookieObjToString(cookie)
     }
     // console.log(options.cookie, headers['Cookie'])
-    if (options.crypto === 'weapi') {
-      headers['User-Agent'] = options.ua || chooseUserAgent('pc')
-      let csrfToken = (headers['Cookie'] || '').match(/_csrf=([^(;|$)]+)/)
-      data.csrf_token = csrfToken ? csrfToken[1] : ''
-      data = encrypt.weapi(data)
-      url = url.replace(/\w*api/, 'weapi')
-    } else if (options.crypto === 'linuxapi') {
-      data = encrypt.linuxapi({
-        method: method,
-        url: url.replace(/\w*api/, 'api'),
-        params: data,
-      })
-      headers['User-Agent'] =
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
-      url = 'https://music.163.com/api/linux/forward'
-    } else if (options.crypto === 'eapi') {
-      const cookie = options.cookie || {}
-      const csrfToken = cookie['__csrf'] || ''
-      const header = {
-        osver: cookie.osver || '17.4.1', //系统版本
-        deviceId: cookie.deviceId || global.deviceId,
-        appver: cookie.appver || iosAppVersion, // app版本
-        versioncode: cookie.versioncode || '140', //版本号
-        mobilename: cookie.mobilename || '', //设备model
-        buildver: cookie.buildver || Date.now().toString().substr(0, 10),
-        resolution: cookie.resolution || '1920x1080', //设备分辨率
-        __csrf: csrfToken,
-        os: cookie.os || 'ios',
-        channel: cookie.channel || '',
-        requestId: `${Date.now()}_${Math.floor(Math.random() * 1000)
-          .toString()
-          .padStart(4, '0')}`,
+
+    let url = '',
+      encryptData = '',
+      crypto = options.crypto,
+      csrfToken = cookie['__csrf'] || ''
+
+    if (crypto === '') {
+      // 加密方式为空，以配置文件的加密方式为准
+      if (APP_CONF.encrypt) {
+        crypto = 'eapi'
+      } else {
+        crypto = 'api'
       }
-      if (cookie.MUSIC_U) header['MUSIC_U'] = cookie.MUSIC_U
-      if (cookie.MUSIC_A) header['MUSIC_A'] = cookie.MUSIC_A
-      headers['Cookie'] = Object.keys(header)
-        .map(
-          (key) =>
-            encodeURIComponent(key) + '=' + encodeURIComponent(header[key]),
-        )
-        .join('; ')
-      data.header = header
-      data = encrypt.eapi(options.url, data)
-      url = url.replace(/\w*api/, 'eapi')
+    }
+
+    // 根据加密方式加密请求数据；目前任意uri都支持四种加密方式
+    switch (crypto) {
+      case 'weapi':
+        headers['Referer'] = 'https://music.163.com'
+        headers['User-Agent'] = options.ua || chooseUserAgent('pc')
+        data.csrf_token = csrfToken
+        encryptData = encrypt.weapi(data)
+        url = APP_CONF.domain + '/weapi/' + uri.substr(5)
+        break
+
+      case 'linuxapi':
+        headers['User-Agent'] =
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
+        encryptData = encrypt.linuxapi({
+          method: method,
+          url: APP_CONF.apiDomain + uri,
+          params: data,
+        })
+        url = 'https://music.163.com/api/linux/forward'
+        break
+
+      case 'eapi':
+      case 'api':
+        // 两种加密方式，都应生成客户端的cookie
+        const cookie = options.cookie || {}
+        const header = {
+          osver: cookie.osver || '17.4.1', //系统版本
+          deviceId: cookie.deviceId || global.deviceId,
+          appver: cookie.appver || iosAppVersion, // app版本
+          versioncode: cookie.versioncode || '140', //版本号
+          mobilename: cookie.mobilename || '', //设备model
+          buildver: cookie.buildver || Date.now().toString().substr(0, 10),
+          resolution: cookie.resolution || '1920x1080', //设备分辨率
+          __csrf: csrfToken,
+          os: cookie.os || 'ios',
+          channel: cookie.channel || '',
+          requestId: `${Date.now()}_${Math.floor(Math.random() * 1000)
+            .toString()
+            .padStart(4, '0')}`,
+        }
+        if (cookie.MUSIC_U) header['MUSIC_U'] = cookie.MUSIC_U
+        if (cookie.MUSIC_A) header['MUSIC_A'] = cookie.MUSIC_A
+        headers['Cookie'] = Object.keys(header)
+          .map(
+            (key) =>
+              encodeURIComponent(key) + '=' + encodeURIComponent(header[key]),
+          )
+          .join('; ')
+
+        let eapi = () => {
+          // 使用eapi加密
+          data.header = header
+          data.e_r =
+            options.e_r != undefined
+              ? options.e_r
+              : data.e_r != undefined
+              ? data.e_r
+              : APP_CONF.encryptResponse // 用于加密接口返回值
+          encryptData = encrypt.eapi(uri, data)
+          url = APP_CONF.apiDomain + '/eapi/' + uri.substr(5)
+        }
+        let api = () => {
+          // 不使用任何加密
+          url = APP_CONF.apiDomain + uri
+          encryptData = data
+        }
+        if (crypto === 'eapi') {
+          eapi()
+        } else if (crypto === 'api') {
+          api()
+        }
+        break
+
+      default:
+        // 未知的加密方式
+        console.log('[ERR]', 'Unknown Crypto:', crypto)
+        break
     }
     const answer = { status: 500, body: {}, cookie: [] }
     // console.log(headers, 'headers')
@@ -136,12 +183,18 @@ const createRequest = (method, url, data = {}, options) => {
       method: method,
       url: url,
       headers: headers,
-      data: new URLSearchParams(data).toString(),
+      data: new URLSearchParams(encryptData).toString(),
       httpAgent: new http.Agent({ keepAlive: true }),
       httpsAgent: new https.Agent({ keepAlive: true }),
     }
 
-    if (options.crypto === 'eapi') settings.encoding = null
+    if (data.e_r) {
+      settings = {
+        ...settings,
+        encoding: null,
+        responseType: 'arraybuffer',
+      }
+    }
 
     if (options.proxy) {
       if (options.proxy.indexOf('pac') > -1) {
@@ -172,12 +225,6 @@ const createRequest = (method, url, data = {}, options) => {
     } else {
       settings.proxy = false
     }
-    if (options.crypto === 'eapi') {
-      settings = {
-        ...settings,
-        responseType: 'arraybuffer',
-      }
-    }
     axios(settings)
       .then((res) => {
         const body = res.data
@@ -185,11 +232,16 @@ const createRequest = (method, url, data = {}, options) => {
           x.replace(/\s*Domain=[^(;|$)]+;*/, ''),
         )
         try {
-          if (options.crypto === 'eapi') {
-            answer.body = JSON.parse(encrypt.decrypt(body))
+          if (data.e_r) {
+            // eapi接口返回值被加密，需要解密
+            answer.body = encrypt.eapiResDecrypt(
+              body.toString('hex').toUpperCase(),
+            )
           } else {
-            answer.body = body
+            answer.body =
+              typeof body == 'object' ? body : JSON.parse(body.toString())
           }
+
           if (answer.body.code) {
             answer.body.code = Number(answer.body.code)
           }
@@ -204,13 +256,8 @@ const createRequest = (method, url, data = {}, options) => {
           }
         } catch (e) {
           // console.log(e)
-          try {
-            answer.body = JSON.parse(body.toString())
-          } catch (err) {
-            // console.log(err)
-            // can't decrypt and can't parse directly
-            answer.body = body
-          }
+          // can't decrypt and can't parse directly
+          answer.body = body
           answer.status = res.status
         }
 
